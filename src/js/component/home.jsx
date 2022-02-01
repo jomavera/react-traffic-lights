04918f5c6f254f31e486acdf4db63a3636ea0b08
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-01 16:47:33 +0000

    Intial comit of traffic lights app

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [selected, setSelected] = useState("");
+	const colors = ["red", "orange", "green"];
+
+	const handler = (color) => {
+		setSelected(color);
+	};
+
+	const circlesHTML = colors.map((e) => {
+		let prendido = "off";
+
+		if (e == selected) {
+			prendido = "on";
+		}
+
+		return (
+			<div className="d-flex justify-content-center">
+				<div className="col-2 rounded p-3 text-light bg-dark align-items-center justify-content-center">
+					<div className="d-flex justify-content-center">
+						<button
+							className={`${e}-circle-${prendido}`}
+							onClick={() => {
+								handler(e);
+							}}></button>
+					</div>
+				</div>
+			</div>
+		);
+	});
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="d-flex align-items-stretch justify-content-center vh-10">
+				<div className="col-sm-1 pt-5 text-light bg-dark"></div>
+			</div>
+			{circlesHTML}
 		</div>
 	);
 };
